5807ae3d5d97cec55b19477a8b993cc439c35611
Houssam GHAZALA <houssem.ghazala@gmail.com>
2015-07-27 21:02:15 +0200

    [java-8] Reorganisation des paquetages.

---
+++ Java-8-Samples/src/fr/houssam/java8/service/IArithmeticOperationsService.java
@@ -0,0 +1,6 @@
+package fr.houssam.java8.service;
+
+public interface IArithmeticOperationsService {
+
+	int uneOperation(int a, int b);
+}
---
+++ Java-8-Samples/src/fr/houssam/java8/service/impl/CalcService.java
@@ -0,0 +1,23 @@
+package fr.houssam.java8.service.impl;
+
+import fr.houssam.java8.service.ICalcService;
+
+public class CalcService {
+
+	public static void main(String[] args) {
+		ICalcService calcService = new ICalcService() {
+
+			@Override
+			public void calc(int a, int b) {
+				System.out.println("a + b = " + (a+b));
+			}
+		};
+		calcService.calc(5, 6);
+
+		ICalcService calcService2 = (a, b) -> {
+			System.out.println("with lambda expression a - b = " + (a - b));
+		};
+		calcService2.calc(20, 6);
+	}
+
+}
