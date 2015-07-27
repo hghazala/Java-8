8dc6c203b51b99ded5cd843b439cf36683271bac
Houssam GHAZALA <houssem.ghazala@gmail.com>
2015-07-27 22:10:32 +0200

    [java-8] exemple avec Runnable interafce.

--- Java-8-Samples/src/fr/houssam/java8/service/impl/RunnableService.java
+++ Java-8-Samples/src/fr/houssam/java8/service/impl/RunnableService.java
@@ -3,7 +3,7 @@ package fr.houssam.java8.service.impl;
 public class RunnableService {
 
 	public static void main(String[] args) {
-		Runnable thread = ()->{System.out.println("coucou");};
+		Runnable thread = ()->{System.out.println("in thread");};
 		thread.run();
 	}
 }
